0d20d68c83869fbaebcd714b0e271c7d20609542
Md Rayhan <mdrayhan9464@gmail.com>
2019-11-27 14:35:21 +0600

    public and static er moddhe difference shiksi

--- javapractice/src/main/java/com/example/javapractice/Car.java
+++ javapractice/src/main/java/com/example/javapractice/Car.java
@@ -2,6 +2,22 @@ package com.example.javapractice;
 
 public class Car {
 
-    public String color = "Black";
+    public String cc_color = "Black";
+    static String cc_interior = "Plastic";
+    public String brand = "Honda";
+    private int noofseat = 5;
+    public Car(String chosencolor, String choseninterior){
+        cc_color = chosencolor;
+        cc_interior = choseninterior;
+    }
 
+    public Car(String chosenbrand, int givennumberseat){
+
+        brand = chosenbrand;
+        noofseat = givennumberseat;
+    }
+
+    public int getNoofseat() {
+        return noofseat;
+    }
 }
--- javapractice/src/main/java/com/example/javapractice/CarSimulator.java
+++ javapractice/src/main/java/com/example/javapractice/CarSimulator.java
@@ -3,9 +3,18 @@ package com.example.javapractice;
 public class CarSimulator {
     public static void main(String[] args) {
         System.out.println("Launching car simulator");
-        //String myCarColor;
-        Car mytoyota = new Car();
-        System.out.println("My car color is "+ mytoyota.color);
+        Car mytoyota = new Car("Orange", "wood");
+        Car myhonda = new Car("Audi", 2);
+        System.out.println(mytoyota.cc_color + "\t" + mytoyota.cc_interior);
+        System.out.println(mytoyota.brand + "\t" + myhonda.getNoofseat());
+        myhonda.cc_interior = "Metal";
+
+        System.out.println(mytoyota.cc_color + "\t" + mytoyota.cc_interior);
+
+
+//        mytoyota.cc_color = "Orange";
+//        System.out.println("My car color is "+ mytoyota.cc_color + " and it has "+ mytoyota.getCc_no_of_seats() + " seats");
+//        System.out.println("My car color is "+ mytoyota.cc_color + " and it has "+ mytoyota.cc_interior + " interior");
 
     }
 }

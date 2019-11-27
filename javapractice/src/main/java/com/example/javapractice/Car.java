16ac18fd452bed29891c9bc4725bbb2f7351eb2b
Md Rayhan <mdrayhan9464@gmail.com>
2019-11-27 10:47:22 +0600

    program start korechi

---
+++ javapractice/src/main/java/com/example/javapractice/Car.java
@@ -0,0 +1,7 @@
+package com.example.javapractice;
+
+public class Car {
+
+    public String color = "Black";
+
+}
---
+++ javapractice/src/main/java/com/example/javapractice/CarSimulator.java
@@ -0,0 +1,11 @@
+package com.example.javapractice;
+
+public class CarSimulator {
+    public static void main(String[] args) {
+        System.out.println("Launching car simulator");
+        //String myCarColor;
+        Car mytoyota = new Car();
+        System.out.println("My car color is "+ mytoyota.color);
+
+    }
+}
